9021b0c943d3c085e3a0e4195e530889f2ea2551
enzodam <166669305+enzodam@users.noreply.github.com>
2025-06-06 20:26:04 -0300

    Update RegistroAjudaDTO.java

--- src/main/java/br/com/fiap/sosclimatech/dto/RegistroAjudaDTO.java
+++ src/main/java/br/com/fiap/sosclimatech/dto/RegistroAjudaDTO.java
@@ -1,20 +1,28 @@
+
 package br.com.fiap.sosclimatech.dto;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegistroAjudaDTO {
+
     private Long id;
+
     @NotNull(message = "ID da pessoa é obrigatório")
     private Long pessoaId;
+
     @NotNull(message = "ID do recurso é obrigatório")
     private Long recursoId;
-    @NotNull(message = "Quantidade é obrigatória")
-    private Integer quantidade;
+
     private LocalDateTime dataRegistro;
+
     private boolean entregue;
+
 }
