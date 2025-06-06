0a39e8e2cb8bd8215e48ed9271f0297da032aa60
enzodam <166669305+enzodam@users.noreply.github.com>
2025-06-06 20:26:35 -0300

    Update RegistroAjuda.java

--- src/main/java/br/com/fiap/sosclimatech/model/RegistroAjuda.java
+++ src/main/java/br/com/fiap/sosclimatech/model/RegistroAjuda.java
@@ -1,9 +1,9 @@
 package br.com.fiap.sosclimatech.model;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import java.time.LocalDateTime;
+
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,25 +11,28 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "TB_REGISTRO_AJUDA")
 public class RegistroAjuda {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_REGISTRO_AJUDA")
     @SequenceGenerator(name = "SQ_REGISTRO_AJUDA", sequenceName = "SQ_REGISTRO_AJUDA", allocationSize = 1)
     @Column(name = "ID_REGISTRO")
     private Long id;
+
     @NotNull 
     @ManyToOne(fetch = FetchType.LAZY) 
     @JoinColumn(name = "ID_PESSOA", nullable = false) 
     private PessoaAfetada pessoa;
+
     @NotNull 
     @ManyToOne(fetch = FetchType.LAZY) 
     @JoinColumn(name = "ID_RECURSO", nullable = false) 
     private Recurso recurso;
-    @NotNull(message = "Quantidade não pode ser nula")
-    @Min(value = 1, message = "Quantidade deve ser pelo menos 1")
-    @Column(name = "NR_QUANTIDADE", nullable = false)
-    private Integer quantidade;
+
     @Column(name = "DT_REGISTRO", nullable = false) 
     private LocalDateTime dataRegistro;
+
     @Column(name = "ST_ENTREGUE", nullable = false)
     private boolean entregue = false; 
+
 }
+
