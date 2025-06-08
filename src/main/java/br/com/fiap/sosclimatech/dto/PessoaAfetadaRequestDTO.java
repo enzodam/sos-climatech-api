669a8ed9eec78bcc8c2d6ece219c7af9c723e3c7
enzodam <166669305+enzodam@users.noreply.github.com>
2025-06-08 13:56:01 -0300

    Update PessoaAfetadaRequestDTO.java

--- src/main/java/br/com/fiap/sosclimatech/dto/PessoaAfetadaRequestDTO.java
+++ src/main/java/br/com/fiap/sosclimatech/dto/PessoaAfetadaRequestDTO.java
@@ -1,22 +1,19 @@
 package br.com.fiap.sosclimatech.dto;
-
+import br.com.fiap.sosclimatech.model.StatusAssistencia;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class PessoaAfetadaRequestDTO {
-        private Long id;
-        @NotBlank(message = "Nome é obrigatório")
-        private String nome;
-        @NotBlank(message = "CPF é obrigatório")
-        @Pattern(regexp = "^\\d{11}$", message = "CPF deve conter 11 dígitos")
-        private String cpf;
-        @NotBlank(message = "Cidade é obrigatória")
-        private String cidade;
-        private Long eventoId;
-}
+import jakarta.validation.constraints.Size;
+public record PessoaAfetadaRequestDTO(
+        @NotBlank(message = "Nome da pessoa não pode ser vazio")
+        @Size(max = 100, message = "Nome da pessoa deve ter no máximo 100 caracteres")
+        String nome,
+        @NotBlank(message = "CPF não pode ser vazio")
+        @Pattern(regexp = "(^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$)|(^\\d{11}$)", message = "CPF inválido. Formato esperado: XXX.XXX.XXX-XX ou XXXXXXXXXXX")
+        String cpf,
+        @NotBlank(message = "Cidade não pode ser vazio")
+        @Size(max = 200, message = "Cidade deve ter no máximo 200 caracteres")
+        String cidade,
+        @NotNull(message = "ID do evento climático relacionado não pode ser nulo")
+        Long eventoId 
+) {}
