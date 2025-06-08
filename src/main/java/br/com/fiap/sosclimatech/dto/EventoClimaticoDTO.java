8bc16e5bcf295851b11bb8c62858224e4c2ba5c6
enzodam <166669305+enzodam@users.noreply.github.com>
2025-06-08 14:23:25 -0300

    Update EventoClimaticoDTO.java

--- src/main/java/br/com/fiap/sosclimatech/dto/EventoClimaticoDTO.java
+++ src/main/java/br/com/fiap/sosclimatech/dto/EventoClimaticoDTO.java
@@ -1,7 +1,6 @@
 package br.com.fiap.sosclimatech.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +15,7 @@ public class EventoClimaticoDTO {
     @NotBlank(message = "Descrição é obrigatória")
     private String descricao;
     @NotNull(message = "Data de início é obrigatória")
-    @PastOrPresent(message = "Data de início deve ser no passado ou presente")
     private LocalDate dataInicio;
-    private LocalDate dataFim;
     @NotNull(message = "Localidade é obrigatória")
     private Long localidadeId;
 }
